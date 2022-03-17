0bf0a8b02a6282458ed4206a31f10ebad537c610
fun <dashags123456789@gmail.com>
2022-03-17 19:39:49 -0400

    changed arguments in testCSearchComparison.sh

--- testCSearchComparison.sh
+++ testCSearchComparison.sh
@@ -52,14 +52,14 @@ if [[ $SMOKESCREEN -eq 1 ]]; then
     n_ways=(2)
     mean_diffs=(0.3)
 else
-    n_iters=(150 250 350)
+    n_iters=(150 250)
     n_runs=100
     kernels=("Matern32")
     func="all"
     noise_levels=(0.1 0.3 0.4)
     exp_biases=(0.1 0.5)
-    n_ways=(2 6 10)
-    mean_diffs=(0.3)
+    n_ways=(2 6 10 20)
+    mean_diffs=(0.0)
     # 0.0 0.3 0.5 1.0 2.0)
     init_samples=(25 100)  # 100 in Nia implementation
 fi
