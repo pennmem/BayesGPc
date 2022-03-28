0b1b26626bdac0bf5e76b1aac1c3ac000e7466cc
fun <dashags123456789@gmail.com>
2022-03-27 22:31:19 -0400

    version.h

--- version.h
+++ version.h
@@ -1,3 +1,3 @@
-#define GIT_BRANCH "refs/heads/bayes_windows"
-#define GIT_COMMIT "7314d63"
+#define GIT_BRANCH "refs/heads/bayes"
+#define GIT_COMMIT "218e48c"
 #define GIT_URL "git@github.com:pennmem/BayesGPc.git"
